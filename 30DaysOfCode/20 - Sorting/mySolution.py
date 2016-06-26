5162e07fc176cb08319d3a7ea8f8f470b5bf125c
Shawn Stern <stern.shawn@gmail.com>
2016-06-25 22:25:02 -0700

    Updating solution to bubble sort to make better use of already provided value n (length of list) and added a break if number of swaps on an iteration is 0 (saves a little bit of time)

--- 30DaysOfCode/20 - Sorting/mySolution.py
+++ 30DaysOfCode/20 - Sorting/mySolution.py
@@ -9,17 +9,24 @@ a = [int(a_temp) for a_temp in input().strip().split(' ')]
 
 # Implement a basic version of Bubble Sort that also tracks the number
 # of swaps executed during the process
-total_swaps = 0;
+total_swaps = 0
 
-for i in range(len(a) - 1):
-    for j in range(len(a) - 1):
+for i in range(n - 1):
+    curr_swaps = 0
+    for j in range(n - 1):
         if a[j] > a[j+1]:
             # Swap
             temp = a[j]
             a[j] = a[j+1]
             a[j+1] = temp
+
+            curr_swaps += 1
             total_swaps += 1
+
+    # If nothing was swapped on this pass, we can assume it's already sorted
+    if curr_swaps == 0:
+        break
 
 print("Array is sorted in " + str(total_swaps) + " swaps.")
 print("First Element: " + str(a[0]))
-print("Last Element: " + str(a[len(a)-1]))
+print("Last Element: " + str(a[n-1]))
