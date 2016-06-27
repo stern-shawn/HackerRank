bafcc6e1d446d66faddec4be449ba25b39b9e0c2
Shawn Stern <stern.shawn@gmail.com>
2016-06-26 17:18:38 -0700

    Utopian Tree code further refined and fully commented

--- General Practice/Utopian Tree/mySolution.py
+++ General Practice/Utopian Tree/mySolution.py
@@ -4,12 +4,16 @@ import sys
 t = int(input().strip())
 for a in range(t):
     n = int(input().strip())
+    # Tree is planted with inital height 1 always
     height = 1
 
+    # Start at 1 since cycle 0 has no effect. Also skips loop if n is 0
     for i in range(1, n+1):
+        # Double height on odd cycles
         if i % 2 != 0:
             height *= 2
-        elif i % 2== 0:
+        # Increase height by 1 on even cycles
+        else:
             height += 1
 
     print(height)
