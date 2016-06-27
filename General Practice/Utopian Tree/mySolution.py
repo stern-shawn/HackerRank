be69e44f7437c8a4e657bf634d6151b2a89b10a8
Shawn Stern <stern.shawn@gmail.com>
2016-06-26 17:14:58 -0700

    Optimizing solution to Utopian Tree problem to use fewer variables and have mure succinct logic

--- General Practice/Utopian Tree/mySolution.py
+++ General Practice/Utopian Tree/mySolution.py
@@ -2,19 +2,14 @@
 import sys
 
 t = int(input().strip())
-for a0 in range(t):
-  n = int(input().strip())
-  height = 1
-  spring = True
+for a in range(t):
+    n = int(input().strip())
+    height = 1
 
-  while n > 0:
-    if spring:
-      height *= 2
-      spring = False
-    else:
-      height += 1
-      spring = True
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            height *= 2
+        elif i % 2== 0:
+            height += 1
 
-    n -= 1
-
-  print(height)
+    print(height)
