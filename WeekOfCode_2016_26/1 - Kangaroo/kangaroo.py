95137b75abe5e2f299a56e696778cf1c08ce73c7
Shawn Stern <stern.shawn@gmail.com>
2016-06-27 22:04:52 -0700

    Optimized Kangaroo solution by approaching the problem algebraically instead of iteratively! Massive runtime savings for larger test cases and cleaner code, get!

--- WeekOfCode_2016_26/1 - Kangaroo/kangaroo.py
+++ WeekOfCode_2016_26/1 - Kangaroo/kangaroo.py
@@ -6,21 +6,9 @@ import sys
 x1,v1,x2,v2 = input().strip().split(' ')
 x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
 
-# Boolean to track if the kangaroos are ever at the same location at the same time
-tied = False
-
-# Locations are constrained between 0 and 10,000 so we only ever need to track up to that distance
-while x1 < 10000 and x2 < 10000:
-    x1 += v1
-    x2 += v2
-
-    if x1 == x2:
-        print("YES")
-        tied = True
-
-        # Also break... just to save some runtime instead of waiting for either value to hit 10k
-        break
-
-# Print NO if no success case occured
-if not tied:
+# Solve the equation x1 + v1 * t = x2 + v2 * t, and make sure we have a
+# positive integer result
+if ((x2 - x1) / (v1 - v2)).is_integer() and (x2 - x1) / (v1 - v2) >= 0:
+    print("YES")
+else:
     print("NO")
