ab514e63ded5cf4be2c1b659a93d6840bb092c40
Shawn Stern <stern.shawn@gmail.com>
2016-06-27 22:08:51 -0700

    Minor clarity update to comments

--- WeekOfCode_2016_26/1 - Kangaroo/kangaroo.py
+++ WeekOfCode_2016_26/1 - Kangaroo/kangaroo.py
@@ -7,7 +7,7 @@ x1,v1,x2,v2 = input().strip().split(' ')
 x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
 
 # Solve the equation x1 + v1 * t = x2 + v2 * t, and make sure we have a
-# positive integer result
+# positive integer result since kangaroos can't travel in partial jumps...
 if ((x2 - x1) / (v1 - v2)).is_integer() and (x2 - x1) / (v1 - v2) >= 0:
     print("YES")
 else:
